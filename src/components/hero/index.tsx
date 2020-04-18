0211eb4792ad0c03f55d93bf90cb2c5e4f202893
Henrique Martins <hmartins224@gmail.com>
2020-04-17 22:35:55 -0300

    Limiting the character's walkable space

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -18,15 +18,15 @@ const Hero = () => {
    const [direction, updateDirectionState] = useState('RIGHT');
 
    useEventListener('keydown', (event: KeyboardEvent<HTMLDivElement>) => {
-      if (event.key === 'ArrowLeft') {
+      if ((event.key === 'ArrowLeft') && (positionState.x > 0)){
         updatePositionState({ x: positionState.x - 1, y: positionState.y });
         updateDirectionState('LEFT');
-      } else if (event.key === 'ArrowRight') {
+      } else if ((event.key === 'ArrowRight') && (positionState.x <= 18)) {
         updatePositionState({ x: positionState.x + 1, y: positionState.y });
         updateDirectionState('RIGHT');
-      } else if (event.key === 'ArrowDown') {
+      } else if ((event.key === 'ArrowDown') && (positionState.y > 1)){
         updatePositionState({ x: positionState.x, y: positionState.y - 1 });
-      } else if (event.key === 'ArrowUp') {
+      } else if ((event.key === 'ArrowUp') && (positionState.y <= 17)){
         updatePositionState({ x: positionState.x, y: positionState.y + 1 });
       }
     });
